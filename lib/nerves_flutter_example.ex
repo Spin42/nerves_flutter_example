a8014bf47bc716e74258ddb959d7b6284be08ee8
Marc Lainez <marc.lainez@gmail.com>
2024-04-30 00:45:23 +0200

    Initial commit

---
+++ lib/nerves_flutter_example.ex
@@ -0,0 +1,18 @@
+defmodule NervesFlutterExample do
+  @moduledoc """
+  Documentation for NervesFlutterExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesFlutterExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_flutter_example_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesFlutterExampleTest do
+  use ExUnit.Case
+  doctest NervesFlutterExample
+
+  test "greets the world" do
+    assert NervesFlutterExample.hello() == :world
+  end
+end
