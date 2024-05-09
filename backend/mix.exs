6228b800456072da23d88a374abcd18cb284ec16
Marc Lainez <marc.lainez@gmail.com>
2024-05-09 13:16:25 +0200

    Add plug cowboy as a dependency

--- backend/mix.exs
+++ backend/mix.exs
@@ -43,7 +43,8 @@ defmodule Backend.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:plug_cowboy, "~> 2.0"},
     ]
   end
 
