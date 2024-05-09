5b1c5a6f3cfa9245f676d9a220a691afd1eb59be
Marc Lainez <marc.lainez@gmail.com>
2024-05-09 12:16:24 +0200

    Remove unneeded import

--- frontend/lib/pages/generator_page.dart
+++ frontend/lib/pages/generator_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../app_state.dart';
 import '../components/bigcard.dart';
-import '../services/favorite_services.dart';
 
 class GeneratorPage extends StatelessWidget {
   @override
